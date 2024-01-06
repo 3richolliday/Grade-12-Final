7976fa0e0a97bc5e80766d454a3eb5b496f5c0e8
Eric Holliday <ericholliday20@gmail.com>
2024-01-06 12:56:48 -0500

    Updated column name and wrote test code to execute.

--- app/assess/routes.py
+++ app/assess/routes.py
@@ -9,7 +9,9 @@ from app.models.user_assessment_detail import User_Assessment_Detail
 @bp.route('/select_language')
 @login_required
 def select_language():
-    for assessment in User_Assessment.query.filter_by(user_id=current_user.id):
-        detail = User_Assessment_Detail.query.filter_by(user_assessment_id=assessment.id).first()
+    assessment = User_Assessment.query.filter_by(user_id=current_user.id).first()
+    
+    for detail in User_Assessment_Detail.query.filter_by(user_assessment_id=assessment.id):
+        x = detail.item.question
 
     return render_template('assess/select_language.html', name=current_user.name)
--- app/models/item.py
+++ app/models/item.py
@@ -4,7 +4,7 @@ class Item(sqla.Model):
     
     id = sqla.Column(sqla.Integer, primary_key=True)
     type = sqla.Column(sqla.String(128)) 
-    text = sqla.Column(sqla.String(128))
+    question = sqla.Column(sqla.String(128))
     answer = sqla.Column(sqla.String(128))
     weight = sqla.Column(sqla.Float)
 
@@ -14,8 +14,8 @@ class Item(sqla.Model):
     def get_type(self):
         return self.type    
     
-    def get_text(self):
-        return self.text
+    def get_question(self):
+        return self.question
     
     def get_answer(self):
         return self.answer
