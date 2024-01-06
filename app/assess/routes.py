cda0f6582882239aa2759c58c2d21572c92e99a7
Eric Holliday <ericholliday20@gmail.com>
2024-01-06 13:36:13 -0500

    First go at looping through questions

--- app/assess/routes.py
+++ app/assess/routes.py
@@ -5,13 +5,26 @@ from flask_login import login_required, current_user
 from app.models.item import Item
 from app.models.user_assessment import User_Assessment
 from app.models.user_assessment_detail import User_Assessment_Detail
+from app.sqla import sqla
+from sqlalchemy import and_
 
 @bp.route('/select_language')
 @login_required
 def select_language():
-    assessment = User_Assessment.query.filter_by(user_id=current_user.id).first()
-    
-    for detail in User_Assessment_Detail.query.filter_by(user_assessment_id=assessment.id):
-        x = detail.item.question
-
     return render_template('assess/select_language.html', name=current_user.name)
+
+@bp.route('/present_question')
+@login_required
+def present_question():
+    assessment = User_Assessment.query.filter(User_Assessment.user_id == current_user.id).first()
+    
+    detail = User_Assessment_Detail.query.filter(
+        (User_Assessment_Detail.user_assessment_id == assessment.id) & 
+        (User_Assessment_Detail.score == None)).first()
+    
+    question = detail.item.question
+
+    detail.score = 0.5
+    sqla.session.commit()
+
+    return render_template('assess/present_question.html', question=question)
